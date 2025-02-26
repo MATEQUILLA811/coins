fcfeb87fa2c1aa2cda6854af7b7f8ad3f9e5c334
MATEQUILLA811 <155582363+MATEQUILLA811@users.noreply.github.com>
2025-02-26 16:28:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from controllers.main_controller import app
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    print("🚀 La aplicación está corriendo en:", os.getenv("REPLIT_APP_URL"))
+    app.run(host="0.0.0.0", port=8080)
+'''if __name__ == "__main__":
+    app.run(debug=True)'''
